213586a25d0ecffd62e90412ed3f27abeeac8675
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2024-01-16 11:25:00 -0500

    :pushpin: Se terminó de mejorar la funcionalidad de las imagenes

--- src/controller/files.controller.js
+++ src/controller/files.controller.js
@@ -1,44 +1,14 @@
 import { FileFetching, URL_API_PRINCIPAL_IMAGES } from "@/fetching/file.fetch";
 import Utils from "@/helpers/helpers";
-import sharp from "sharp";
-import { env } from "../../next.config";
 import fetch from "node-fetch";
 import path from "path";
 import fs from "fs";
+import Jimp from "jimp";
 
 export class FilesController {
   static async apiFileImageSave(req, res) {
     try {
-      const cookie = req.cookies[env.server.cookies.main_cookie.name];
-      const image = req?.file;
-      if (!image || !image.mimetype.startsWith("image/"))
-        return res
-          .status(400)
-          .json(Utils.Message(true, 400, "Archivo inválido"));
-      let imageBuffer = image.buffer;
-      const targetSizeKB = 50; // Tamaño máximo deseado en KB
-      let quality = 100; // Calidad inicial al 100%
-      while (imageBuffer.length > targetSizeKB * 1000 && quality > 10) {
-        // Redefine la calidad para reducir el tamaño de archivo
-        quality -= 10;
-
-        // Redimensiona la imagen y ajusta la calidad
-        const { width, height } = await sharp(imageBuffer).metadata();
-        const newWidth = Math.round((width * quality) / 100);
-        const newHeight = Math.round((height * quality) / 100);
-        const processedImageBuffer = await sharp(imageBuffer)
-          .resize(newWidth, newHeight)
-          .jpeg({ quality })
-          .toBuffer();
-
-        // Actualiza el búfer de imagen original
-        imageBuffer = processedImageBuffer;
-      }
-      const formImageData = new FormData();
-      const imageBlob = new Blob([imageBuffer], { type: "image/jpeg" });
-      formImageData.append("file", imageBlob, "bufferImageReducer.jpeg");
-      const sendImage = await FileFetching.setFile(formImageData, cookie);
-      return res.json(sendImage);
+      throw new Error("Error, funcionalidad deshabilitada");
     } catch (error) {
       console.log(error);
       return res.json(Utils.Message(true, 500, "Server Error"));
@@ -73,9 +43,16 @@ export class FilesController {
 
   static async apiAsyncSaveImage(image, nameImage, cookie) {
     try {
+      let imageSend;
+      if (image.mimetype != Jimp.MIME_JPEG) {
+        const convertToJPEG = await Jimp.read(image.buffer);
+        const jpegBuffer = await convertToJPEG
+          .quality(90)
+          .getBufferAsync(Jimp.MIME_JPEG);
+        imageSend = jpegBuffer;
+      } else imageSend = image.buffer;
       const formImageData = new FormData();
-      const imageBlob = new Blob([image], { type: image.mimetype });
-      console.log(imageBlob);
+      const imageBlob = new Blob([imageSend], { type: Jimp.MIME_JPEG });
       formImageData.append("file", imageBlob, nameImage);
       formImageData.append("fileName", nameImage);
       const saveImage =
