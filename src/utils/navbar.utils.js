92b4eedb38cd77fe67e25f19b474063867ba3d1a
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2024-02-03 17:51:03 -0500

    :tada: Se finalizó el modulo de barras de navegación

--- src/utils/navbar.utils.js
+++ src/utils/navbar.utils.js
@@ -1,25 +1,9 @@
-import { CONST_NAVBAR_ICON_DEFAULT, CONST_NAVBAR_LIST_ICONS } from "@/constants/navbar.constant";
-import { CONST_NAVBAR_USERS, CONST_USER_UA } from "@/constants/user.constant";
+import {
+  CONST_NAVBAR_ICON_DEFAULT,
+  CONST_NAVBAR_LIST_ICONS,
+} from "@/constants/navbar.constant";
 
 export class NavbarUtils {
-  static getNavbarUser(idRoleUser = CONST_USER_UA.id) {
-    try {
-      idRoleUser = idRoleUser || CONST_USER_UA.id;
-      const navbarUser = CONST_NAVBAR_USERS.find(({ idRole }) =>
-        idRole.find((id) => id === idRoleUser)
-      );
-      if (!navbarUser)
-        return CONST_NAVBAR_USERS.find(({ idRole }) =>
-          idRole.find((id) => id === CONST_USER_UA.id)
-        );
-      return navbarUser;
-    } catch (error) {
-      return CONST_NAVBAR_USERS.find(({ idRole }) =>
-        idRole.find((id) => id === CONST_USER_UA.id)
-      );
-    }
-  }
-
   static getIcon(icon) {
     const findIcon = CONST_NAVBAR_LIST_ICONS.find(({ name }) => name === icon);
     if (findIcon) return findIcon.icon;
