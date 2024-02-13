afd784466de5e2c404803847d7b0e81d9e51748e
JuanTG08 <64704012+JuanTG08@users.noreply.github.com>
2024-02-13 10:09:38 -0500

    :camera_flash: Se cambio el texto para el overdue-payments

--- src/pages/overdue-payments/index.js
+++ src/pages/overdue-payments/index.js
@@ -1,16 +1,10 @@
 import HeaderPage from "@/components/views/partials/HeaderPage";
 import { Button } from "@nextui-org/react";
-import { getSession, useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
 
 const ViewOverduePayments = () => {
-  const { data: session } = useSession();
-  useEffect(() => {
-    console.log(session)
-    // if (window && session) signOutGoogle();
-  }, [session]);
   return (
     <>
       <HeaderPage title="Falta de pago" />
@@ -31,18 +25,12 @@ const ViewOverduePayments = () => {
               style={{ textWrap: "balance" }}
               className="text-4xl text-center sm:text-5xl lg:text-5xl p-0 m-0 font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight xl:leading-tight"
             >
-              Falta de Pago del Conjunto Residencial
+              Oops, parece que ha habido un problema.
             </h1>
           </div>
-          <p
-            className="text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl"
-          >
-            Oops, parece que ha habido un problema. El conjunto residencial no
-            ha realizado los pagos necesarios, lo que ha resultado en la
-            suspensión temporal de su acceso. Si cree que esto podría ser un
-            error, por favor, póngase en contacto con el administrador del
-            conjunto para resolver este problema y restablecer el acceso a los
-            servicios.
+          <p className="text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl">
+            Servicio no disponible relacionados a temas de facturación y pagos,
+            contacta tu administración.
           </p>
           <Button
             color="primary"
